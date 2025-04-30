40ac3ebaeb2a85bb4a7ef713c86a8096a1bbd9b8
BretRen <Bret_scratch@outlook.com>
2025-04-30 09:26:16 +0000

    111

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-/*!
- * express
- * Copyright(c) 2009-2013 TJ Holowaychuk
- * Copyright(c) 2013 Roman Shtylman
- * Copyright(c) 2014-2015 Douglas Christopher Wilson
- * MIT Licensed
- */
+const express = require('express')
+const app = express()
+const port = 3000
 
-'use strict';
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-module.exports = require('./lib/express');
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
